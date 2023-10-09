af7fbde24dd20fac69f07f93d69e69bf3bc66c57
Jorubova Albina <deletawer@gmail.com>
2023-10-09 21:55:58 +0300

    glavnaya

--- messenger/messenger/Form1.Designer.cs
+++ messenger/messenger/Form1.Designer.cs
@@ -181,7 +181,7 @@
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "я";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
