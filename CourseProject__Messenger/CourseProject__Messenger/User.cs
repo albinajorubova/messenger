c9c5784f3888338203c8d601661e9c9f6692695c
Jorubova Albina <deletawer@gmail.com>
2023-12-03 14:30:14 +0300

    dialogs

--- CourseProject__Messenger/CourseProject__Messenger/User.cs
+++ CourseProject__Messenger/CourseProject__Messenger/User.cs
@@ -20,6 +20,13 @@ namespace CourseProject__Messenger.usercontrols
             UserID = UserID;
         }
 
+        public class DialogInfo
+        {
+            public int DialogID { get; set; }
+            public string DialogName { get; set; }
+            public List<Usercontrols.FriendInfo> Participants { get; set; }
+        }
+
         public class FriendInfo
         {
             public string Name { get; set; }
@@ -158,6 +165,106 @@ namespace CourseProject__Messenger.usercontrols
             }
 
             return friendsList;
+        }
+
+        public List<DialogInfo> GetDialogsListWithInfo(string email)
+        {
+            List<DialogInfo> dialogsList = new List<DialogInfo>();
+
+            try
+            {
+                string connectionString = "Server=127.0.0.1;Port=3306;Database=messenger;Uid=root;";
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string getUserIdQuery = "SELECT UserID FROM Users WHERE Email = @Email";
+                    MySqlCommand getUserIdCommand = new MySqlCommand(getUserIdQuery, connection);
+                    getUserIdCommand.Parameters.AddWithValue("@Email", email);
+                    object userIdResult = getUserIdCommand.ExecuteScalar();
+
+                    if (userIdResult != null)
+                    {
+                        int userId = Convert.ToInt32(userIdResult);
+
+                        string getDialogsQuery = "SELECT d.DialogID, d.Name FROM Dialogs d JOIN DialogParticipants dp ON d.DialogID = dp.DialogID WHERE dp.UserID = @UserID";
+                        MySqlCommand getDialogsCommand = new MySqlCommand(getDialogsQuery, connection);
+                        getDialogsCommand.Parameters.AddWithValue("@UserID", userId);
+
+                        using (MySqlDataReader reader = getDialogsCommand.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                int dialogID = reader.GetInt32(0);
+                                string dialogName = reader.GetString(1);
+
+                                var dialogInfo = new DialogInfo
+                                {
+                                    DialogID = dialogID,
+                                    DialogName = dialogName,
+                                    Participants = GetDialogParticipants(dialogID)
+                                };
+
+                                dialogsList.Add(dialogInfo);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Пользователь с почтой {email} не найден.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+
+            return dialogsList;
+        }
+
+        public List<FriendInfo> GetDialogParticipants(int dialogID)
+        {
+            List<FriendInfo> participants = new List<FriendInfo>();
+
+            try
+            {
+                string connectionString = "Server=127.0.0.1;Port=3306;Database=messenger;Uid=root;";
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // Получаем список участников диалога по его DialogID
+                    string getParticipantsQuery = "SELECT u.Name, u.Email FROM DialogParticipants dp JOIN Users u ON dp.UserID = u.UserID WHERE dp.DialogID = @DialogID";
+                    MySqlCommand getParticipantsCommand = new MySqlCommand(getParticipantsQuery, connection);
+                    getParticipantsCommand.Parameters.AddWithValue("@DialogID", dialogID);
+
+                    using (MySqlDataReader reader = getParticipantsCommand.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string participantName = reader.GetString(0);
+                            string initials = GetInitials(participantName);
+                            string participantEmail = reader.GetString(1);
+
+                            var participantInfo = new FriendInfo
+                            {
+                                Name = participantName,
+                                Initials = initials,
+                                Email = participantEmail
+                            };
+
+                            participants.Add(participantInfo);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+
+            return participants;
         }
     }
 }
